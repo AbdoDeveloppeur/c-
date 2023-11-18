580ea30709796dbc81a07349ca730f1abde67aee
AbdoDeveloppeur <abdelazizeljazouli822@gmail.com>
2023-11-18 18:27:27 +0100

    v4

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+
+            Console.ReadKey();
+            
+      
+        }
+
+         //function to somme two numbers
+        static void somme()
+        {
+            int a = 10;
+            int b = 5;
+
+
+
+            Console.WriteLine("Modulo Equal :"+ a%b);
+
+        }
+    }
+}
